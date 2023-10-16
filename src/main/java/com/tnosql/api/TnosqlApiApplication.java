deea3aba023a691925e10cc763085cdfc8b2d49e
wilson arriola <warriol@gmail.com>
2023-10-15 23:57:26 -0300

    api rest para personas

---
+++ src/main/java/com/tnosql/api/TnosqlApiApplication.java
@@ -0,0 +1,13 @@
+package com.tnosql.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnosqlApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TnosqlApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tnosql/api/TnosqlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnosql.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TnosqlApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
